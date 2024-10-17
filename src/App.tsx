8f6616731c8b1649cb8cfb1f96b39580fc48c2a0
aniket <aniket.umbc@gmail.com>
2024-10-17 06:11:39 +0530

    added text prod

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ const App = () => (
     <>
         <div>
             <h1> Awesome, Welcome to React Scratch Page </h1>
-
             <h2> Vercel Build implemented </h2>
+            <h2> Production test env </h2>
         </div>
     </>
 );
